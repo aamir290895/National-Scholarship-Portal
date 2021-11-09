070521a8acb5c7accce8d1397eb06632ff10fd00
aamir290895 <aamir.007sdl.ak@gmail.com>
2021-11-09 11:05:55 +0530

    09/11/21

---
+++ NationalScholarshipPortal/src/test/java/com/nsp/project/NationalScholarshipPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsp.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationalScholarshipPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
