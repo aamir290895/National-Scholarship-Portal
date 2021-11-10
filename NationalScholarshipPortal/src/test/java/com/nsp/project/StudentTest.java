08123ab4a05d2949b4608f17601d50371ed3d156
aamir290895 <aamir.007sdl.ak@gmail.com>
2021-11-10 19:02:39 +0530

    ipdated

--- NationalScholarshipPortal/src/test/java/com/nsp/project/StudentTest.java
+++ NationalScholarshipPortal/src/test/java/com/nsp/project/StudentTest.java
@@ -1,5 +1,10 @@
 package com.nsp.project;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,15 +14,12 @@ import com.nsp.project.repository.StudentRepositoryImpl;
 
 @SpringBootTest
 public class StudentTest {
-	@Autowired
-	StudentRepositoryImpl sImpl;
+	@PersistenceContext
+	EntityManager entityManager;
 	
 	@Test
 	void insertDetails() {
-		Student s = new Student();
-		s.setStudentId( (long) 12);
-		sImpl.count();
-		
+		EntityManagerFactory emFactory = Persistence.createEntityManagerFactory(null, null);
 		
 	}
 
