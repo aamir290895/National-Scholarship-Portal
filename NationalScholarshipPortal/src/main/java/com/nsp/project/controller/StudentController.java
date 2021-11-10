e1122b1a0a28cdd0f7124e308b133ee9f41ac6bd
aamir290895 <aamir.007sdl.ak@gmail.com>
2021-11-10 16:41:12 +0530

    updated

--- NationalScholarshipPortal/src/main/java/com/nsp/project/controller/StudentController.java
+++ NationalScholarshipPortal/src/main/java/com/nsp/project/controller/StudentController.java
@@ -1,12 +1,25 @@
 package com.nsp.project.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nsp.project.entity.Student;
+import com.nsp.project.repository.StudentRepository;
 
 @RestController
 @RequestMapping("/Student")
 public class StudentController {
 	
-	
-
+	  @Autowired
+      private StudentRepository studentRepository;
+	  
+	  @GetMapping("/list")
+	  public List<Student> getAllStudents(){
+		return studentRepository.findAll();
+		  
+	  }
 }
