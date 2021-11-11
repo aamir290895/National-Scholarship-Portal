549b9acd76f61ed7e1a4bd5d682b04d4d9395dd4
aamir290895 <aamir.007sdl.ak@gmail.com>
2021-11-11 23:00:56 +0530

    updated

--- NationalScholarshipPortal/src/test/java/com/nsp/project/StudentTest.java
+++ NationalScholarshipPortal/src/test/java/com/nsp/project/StudentTest.java
@@ -1,10 +1,7 @@
 package com.nsp.project;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.nsp.project.entity.Student;
 
 @SpringBootTest
 public class StudentTest {
