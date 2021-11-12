b2e0814a81890a56c10e1869a5ed0f66ade4551e
aamir290895 <aamir.007sdl.ak@gmail.com>
2021-11-13 00:16:16 +0530

    updated

--- NationalScholarshipPortal/src/test/java/com/nsp/project/NationalScholarshipPortalApplicationTests.java
+++ NationalScholarshipPortal/src/test/java/com/nsp/project/NationalScholarshipPortalApplicationTests.java
@@ -11,38 +11,38 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 
 import com.nsp.project.entity.Student;
+import com.nsp.project.repository.StudentRepositoryImpl;
 import com.nsp.project.service.StudentServiceImpl;
 
 @SpringBootTest
 class NationalScholarshipPortalApplicationTests {
 
 
-	
+	@Autowired
+	private StudentRepositoryImpl repo;
 
 	@Test
 	void contextLoads() {
 
 	}
 
-//	@Test
-//	public void insertDetails() {
-//		HibernateTemplate t;
-//		LocalDate ld = LocalDate.of(2021, 10, 11);
-//
-//		Student s = new Student();
-//		s.setStudentId((long) 12345);
-//		s.setAdharNumber("462773832833");
-//		s.setBankAccountNumber("20380162843");
-//		s.setStudentName("Aamir Khan");
-//		s.setBankIfsc("SBIN0000481");
-//		s.setGender("Male");
-//		s.setDistrict("Shahdol");
-//		s.setEmail("aamir.007sdl.ak@gmail.com");
-//		s.setBankName("SBI");
-//		s.setDateOfBirth(ld);
-//		s.setInstituteCode(1);
-//
-//		t.save(s);
-//
-//	}
+	@Test
+	public void insertDetails() {
+		LocalDate ld = LocalDate.of(2021, 10, 11);
+
+		Student s = new Student();
+		s.setStudentId((long) 12345);
+		s.setAdharNumber("462773832833");
+		s.setBankAccountNumber("20380162843");
+		s.setStudentName("Aamir Khan");
+		s.setBankIfsc("SBIN0000481");
+		s.setGender("Male");
+		s.setDistrict("Shahdol");
+		s.setEmail("aamir.007sdl.ak@gmail.com");
+		s.setBankName("SBI");
+		s.setDateOfBirth(ld);
+		s.setInstituteCode(1);
+
+        repo.save(s);
+	}
 }
